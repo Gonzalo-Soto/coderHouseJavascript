138ff95dc25205c36bb4278ef8574e521b63331a
Gonzalo <programadorgs@gmail.com>
2022-05-11 19:46:11 -0300

    cree index, carpeta css, imagens, js armando la estructura para furuto, agrege js con ejercicio

---
+++ js/Entregable11-5.js
@@ -0,0 +1,20 @@
+    let numeroSolicitado = parseInt(prompt("Ingresar numero"));
+
+    for (numeroSolicitado = numeroSolicitado; numeroSolicitado <100; numeroSolicitado++){
+
+    console.log("El numero ingresado + 1 es: " + numeroSolicitado);}
+
+    if (numeroSolicitado == 100){
+        
+        alert("al fin deje de hacer algoritmos infinitos")
+    }
+
+
+
+    let contrasenia = "";
+
+    while(contrasenia != 1234){
+        contrasenia = prompt("introduzca su contraseña")
+    }
+
+    alert("ingresaste las contraseña correcta")
